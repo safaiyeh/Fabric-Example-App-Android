231685748cfb2be6996d9cfe15d6f3fd8cd5f425
Jason Safaiyeh <jsafaiyeh@gmail.com>
2015-07-20 00:28:12 -0700

    Added Crashlytics

--- app/src/main/java/com/jsafaiyeh/fabricexampleapp/MainActivity.java
+++ app/src/main/java/com/jsafaiyeh/fabricexampleapp/MainActivity.java
@@ -4,12 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
     }
 }
